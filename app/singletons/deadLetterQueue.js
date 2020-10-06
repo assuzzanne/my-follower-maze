a0a32d86f4548ce7101f84fe2a173c87241b6f0e
Aude Rouaux <aude.s.rouaux@gmail.com>
2020-10-06 22:43:35 +0200

    feat(deadLetterQueueInstance): remove useless param in function peek

--- app/singletons/deadLetterQueue.js
+++ app/singletons/deadLetterQueue.js
@@ -2,7 +2,7 @@ const deadLetterQueue = [];
 const deadLetterQueueInstance = {
   enqueue: (event) => deadLetterQueue.push(event),
   dequeue: (event) => deadLetterQueue.shift(event),
-  peek: (event) =>
+  peek: () =>
     !deadLetterQueue.isEmpty() ? deadLetterQueue[0] : undefined,
   isEmpty: () => deadLetterQueue.length == 0,
   size: () => deadLetterQueue.length,
