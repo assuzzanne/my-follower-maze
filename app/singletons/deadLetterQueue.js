770de0ded69d98907630d57ce7e60dd20cee67b0
Aude Rouaux <aude.s.rouaux@gmail.com>
2020-10-08 23:23:39 +0200

    feat(deadLetterQueue): add get function

--- app/singletons/deadLetterQueue.js
+++ app/singletons/deadLetterQueue.js
@@ -2,10 +2,10 @@ const deadLetterQueue = [];
 const deadLetterQueueInstance = {
   enqueue: (event) => deadLetterQueue.push(event),
   dequeue: (event) => deadLetterQueue.shift(event),
-  peek: () =>
-    !deadLetterQueue.isEmpty() ? deadLetterQueue[0] : undefined,
+  peek: () => (!deadLetterQueue.isEmpty() ? deadLetterQueue[0] : undefined),
   isEmpty: () => deadLetterQueue.length == 0,
   count: () => deadLetterQueue.length,
+  get: () => console.log(deadLetterQueue),
 };
 Object.freeze(deadLetterQueueInstance);
 
