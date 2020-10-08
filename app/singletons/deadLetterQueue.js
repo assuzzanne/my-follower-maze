ab592787003460805a692cd116b687699363b3cb
Aude Rouaux <aude.s.rouaux@gmail.com>
2020-10-09 01:31:20 +0200

    feat(deadLetterQueue): add function get and function clearOut

--- app/singletons/deadLetterQueue.js
+++ app/singletons/deadLetterQueue.js
@@ -1,12 +1,28 @@
 const deadLetterQueue = [];
+
+function addToDeadLetterQueue(event) {
+  deadLetterQueueInstance.enqueue(event);
+  console.info(
+    "Message added to the dead letter queue successfully!",
+    event,
+    deadLetterQueueInstance.count()
+  );
+}
+
 const deadLetterQueueInstance = {
   enqueue: (event) => deadLetterQueue.push(event),
   dequeue: (event) => deadLetterQueue.shift(event),
   peek: () => (!deadLetterQueue.isEmpty() ? deadLetterQueue[0] : undefined),
   isEmpty: () => deadLetterQueue.length == 0,
   count: () => deadLetterQueue.length,
-  get: () => console.log(deadLetterQueue),
+  get: () => deadLetterQueue,
+  clearOut: () => {
+    while (deadLetterQueue.length > 0) {
+      deadLetterQueue.pop();
+    }
+  },
 };
 Object.freeze(deadLetterQueueInstance);
 
 exports.deadLetterQueueInstance = deadLetterQueueInstance;
+exports.addToDeadLetterQueue = addToDeadLetterQueue;
